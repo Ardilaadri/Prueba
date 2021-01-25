ec6c82d2d67dc6c8f1639fd8e054fe05a04b3ce5
Adriana Ardila <adriana2014lucia@outlook.com>
2021-01-24 20:25:09 -0500

    all

---
+++ suggestions.js
@@ -0,0 +1,13 @@
+let suggestions = [
+  "OneWord",
+  "twoWord",
+  "threeWord",
+  "fourWord",
+  "fiveWord",
+  "sixWord",
+  "sevenWord",
+  "eightWord",
+  "nineword",
+  "tenword",
+  "example",
+];
